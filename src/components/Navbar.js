a7677049d7da962b2fb5bba4af46c1e5fa2d56d0
unknown <luckyritesh132@gmail.com>
2023-06-14 11:28:41 +0530

    change projet name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -56,7 +56,7 @@ function Navbar() {
             <AppBar sx={{ backgroundColor: 'white' }}>
                 <Toolbar>
                     <Typography sx={{ color: '#e80071' }}>
-                        NYKAA
+                        NYKAA By Ritesh
                     </Typography>
                     <Tabs>
                         {menuarr.map((menu, i) => {
